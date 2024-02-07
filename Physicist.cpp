9fef197610d379c1b993dc0a2c65b5f68e15c97d
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-02-08 02:01:16 +0530

    nice

---
+++ Physicist.cpp
@@ -0,0 +1,14 @@
+//Problem:
+//Name:
+//Author:Vedant Borkar
+#include<iostream>  
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#define ll = long long;
+using namespace std;
+int main(){
+
+
+  return 0;
+}
