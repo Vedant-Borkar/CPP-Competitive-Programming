444fa15e28570546cd35cd3b150a0e73f73e7c3c
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-02-09 03:06:06 +0530

    niceee

--- Drinks.py
+++ Drinks.py
@@ -1,4 +1,9 @@
 #Problem:https://codeforces.com/problemset/problem/122/A
 #Name:Lucky Division
 #Author:Vedant Borkar
-def
+def drinks(n):
+  list1=[int(x) for x in input().split()]
+  sum1=sum(list1)
+
+n=int(input())
+drinks(n)
