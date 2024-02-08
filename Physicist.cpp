f54759c70b52a215eb3d67544e04c80ae62476cd
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-02-09 00:31:00 +0530

    begin

--- Physicist.cpp
+++ Physicist.cpp
@@ -1,5 +1,5 @@
-//Problem:
-//Name:
+//Problem:https://codeforces.com/problemset/problem/69/A
+//Name:Young Physicist
 //Author:Vedant Borkar
 #include<iostream>  
 #include<algorithm>
@@ -8,7 +8,23 @@
 #define ll = long long;
 using namespace std;
 int main(){
-
-
+  int n;
+  cin>>n;
+  int arr[n][3];
+  for(int i=0;i<n;i++){
+    cin>>arr[i][0];
+    cin>>arr[i][1];
+    cin>>arr[i][2];
+  }
+  int sumX=0,sumY=0,sumZ=0;
+  for(int i=0;i<n;i++){
+    sumX+=arr[i][0];
+    sumY+=arr[i][1];
+    sumZ+=arr[i][2];
+  }
+  if((sumX==0) && (sumY==0) && (sumZ==0))
+    cout<<"YES";
+  else
+    cout<<"NO";
   return 0;
 }
