e7af59eb6b8f247c544f274f9bd0a3f2f0bedbc5
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-02-09 03:17:32 +0530

    changes

--- Drinks.py
+++ Drinks.py
@@ -1,9 +1,9 @@
 #Problem:https://codeforces.com/problemset/problem/122/A
-#Name:Lucky Division
+#Name:Drinks
 #Author:Vedant Borkar
 def drinks(n):
   list1=[int(x) for x in input().split()]
-  sum1=sum(list1)
-
+  sum1=sum(list1)/(100*n)
+  print(sum1*100)
 n=int(input())
 drinks(n)
---
+++ Expression.py
@@ -0,0 +1,17 @@
+#Problem:https://codeforces.com/problemset/problem/479/A
+#Name:Expression
+#Author:Vedant Borkar
+def exp(a,b,c):
+  x=[0,0,0,0,0,0]
+  x[0]=a+b+c
+  x[1]=a*b*c
+  x[2]=(a+b)*c
+  x[3]=(a*b)+c
+  x[4]=(a+(b*c))
+  x[5]=(a*(b+c))
+  ans=max(x)
+  print(ans)
+a=int(input())
+b=int(input())
+c=int(input())
+exp(a,b,c)
