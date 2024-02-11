445a98f1db0fc23b21376fbb1c7f4014c7d65500
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-02-11 13:03:20 +0530

    1100 are tricky

--- CPP Python/Coprime.py
+++ CPP Python/Coprime.py
@@ -9,7 +9,8 @@ def coprime(n,max):
       if(gcd(l[i],l[j])==1):
         if((i+j)>max):
           max=i+j
-  print(max+2)  
+  if(max>-1):print(max+2)  
+  else: print("-1")
 t=int(input())
 while(t>0):
   coprime(int(input()),-1)
---
+++ Coprime.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int coprime(int n, int max) {
+    vector<int> l(n);
+    for(int i=0; i<n; i++)
+        cin >> l[i];
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(__gcd(l[i],l[j])==1) {
+                if((i+j)>max)
+                    max=i+j;
+            }
+        }
+    }
+    if(max>-1)
+        cout << max+2 << "\n";
+    else
+        cout << "-1" << "\n";
+    return 0;
+}
+int main() {
+    int t,n;
+    cin >> t;
+    while(t--) {cin>>n;
+        coprime(n,-1);
+    }
+    return 0;
+}
