9487d981a957e8194e05eb0dfda548e9fe952e7e
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-02-14 02:02:57 +0530

    Lol

--- Taxi.cpp
+++ Taxi.cpp
@@ -1,5 +1,5 @@
-//Problem:
-//Name:
+//Problem:https://codeforces.com/problemset/problem/158/B
+//Name:Taxi
 //Author:Vedant Borkar
 #include<bits/stdc++.h>
 #define ll = long long;
@@ -7,10 +7,12 @@ using namespace std;
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  int i,n,j,k,t,count=0,sum=0,arr[n];
-  cin>>n;cin>>t;
+  int i,n,j,k,t,count=0;double sum=0;
+  cin>>n;int arr[n];
   for(i=0;i<n;i++){
     cin>>arr[i];
-  }
+    sum+=arr[i];
+  }double ans=sum/4;
+  cout<<ceil(ans);
   return 0;
 }
