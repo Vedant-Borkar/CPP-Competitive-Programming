9b5b1ee562ee22a816c405203c8e108c271828dd
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-02-20 22:24:41 +0530

    siuu

--- VladShapes.cpp
+++ VladShapes.cpp
@@ -4,30 +4,38 @@
 #include<bits/stdc++.h>
 #define ll = long long;
 using namespace std;
-int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  int i,n,j,k,t,count=0,sum=0;
-  cin>>t;
-  while(t-->0){
-    cin>>n;
-    vector<string> arr(n);
-    for(i=0;i<n;i++){
-          cin>>arr[i];
+bool isSquare(int n){
+  int arr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+          cin>>arr[i][j];
       }
-    bool square=true;
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
+    }
+   bool square=true;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
           if(arr[i][j]==1){
-            if(i+1<n && j+1<n && arr[i+1][j] == '1' && arr[i][j+1] == '1'){
-              square=true;
-              break;
-            }
-            else
-            square=false;
+            if(arr[i][j+1]!=1 && arr[i+1][j]==1){
+                square=false;
+                return square;
+              }else if(arr[i][j+1]==1 && arr[i+1][j]!=1){
+                square=false;
+                return square;
+              }
+              else
+                return square;
           }
         }
     }
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int i,n,j,k,t,count=0,count1=0,sum=0;
+  cin>>t;
+  while(t-->0){
+    cin>>n;
+    bool square = isSquare(n);
     if(square==true)
       cout<<"SQUARE\n";
     else
@@ -35,3 +43,16 @@ int main(){
   }
   return 0;
 }
+/**
+ * 
+*/
+/**
+ * if(count>0){
+          if(arr[i][j]==1){
+            count1++;break;
+          }
+        }
+         else if(arr[i][j]==1){
+            count++;
+          }
+*/
