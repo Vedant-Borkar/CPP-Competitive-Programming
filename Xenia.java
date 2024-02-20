0be8b59b2cd7c84ca1a568dd923c204196a72ea0
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-02-20 23:21:18 +0530

    SIUUU

---
+++ Xenia.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt(); // Number of houses
+        int m = scanner.nextInt(); // Number of tasks
+
+        int[] tasks = new int[m];
+        for (int i = 0; i < m; i++) {
+            tasks[i] = scanner.nextInt();
+        }
+
+        long time = 0;
+        int currentHouse = 1;
+
+        for (int i = 0; i < m; i++) {
+            int destination = tasks[i];
+
+            if (destination >= currentHouse) {
+                time += destination - currentHouse;
+            } else {
+                time += n - currentHouse + destination;
+            }
+
+            currentHouse = destination;
+        }
+
+        System.out.println(time);
+
+        scanner.close();
+    }
+}
