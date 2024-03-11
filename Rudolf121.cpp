d9141180e634765fd50d1059ec8a176f86fdfee2
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-03-11 21:20:36 +0530

    SIUUU

--- Rudolf121.cpp
+++ Rudolf121.cpp
@@ -11,6 +11,26 @@ int main(){
   cin>>t;
   while(t-->0){
     cin>>n;
+    long a[n];
+    for(i=0;i<n;i++)
+        cin>>a[i];
+    for(i=n-2;i>0;i--){
+        long dec=a[i+1];
+        if(dec<0) continue;
+        a[i+1]=0;
+        a[i]-=2*dec;
+        a[i-1]-=dec;
+    }
+    count=0;
+    for(i=0;i<n;i++){
+        if(a[i]!=0){
+            cout<<"NO\n";
+            count++;
+            break;
+        }
+    }
+    if(count==0)
+    cout<<"YES\n";
   }
   return 0;
 }
