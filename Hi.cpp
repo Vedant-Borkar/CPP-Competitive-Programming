5212a330a4240e8f83640a426284cee91d60d75a
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-05-30 16:59:10 +0530

    Push

---
+++ Hi.cpp
@@ -0,0 +1,17 @@
+//Problem:
+//Name:
+//Author:Vedant Borkar
+#include<bits/stdc++.h>
+#define ll = long long;
+using namespace std;
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int i,n,j,k,t,count=0,sum=0;
+  cin>>t;
+  while(t-->0){
+    //cin>>n;
+    cout<<"Hello";
+  }
+  return 0;
+}
