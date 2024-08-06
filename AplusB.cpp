c2f898534512be45e0573a2fb7b0c45e4ff738c8
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-08-07 00:36:45 +0530

    Div 4 CF round 964

---
+++ AplusB.cpp
@@ -0,0 +1,18 @@
+//Problem:https://codeforces.com/contest/1999/submit
+//Name:A. A+B Again?
+//Author:Vedant Borkar
+#include<bits/stdc++.h>
+#define ll = long long;
+using namespace std;
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int i,n,j,k,t,sum=0;
+  cin>>t;
+  while(t-->0){
+    //int count=0;
+    cin>>n;
+    cout<<(n%10 + n/10)<<"\n";
+  }
+  return 0;
+}
